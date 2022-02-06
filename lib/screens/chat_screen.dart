6ff216cf308b5d0afe4d4e57a56e1e5d4d1a38a3
yejun_1220@naver.com <yejun_1220@naver.com>
2022-02-07 02:58:13 +0900

    Flutter Chat 기능 구현
    initState()
    !,? 활용법

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _authentication = FirebaseAuth.instance;
+  User? loggedUser; // 값을 초기화하지 않을 것이므로 ? 을 붙여야 한다.
+
+  @override
+  void initState() { // 위젯이 생성될 때 처음으로 호출되는 메서드다.
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() {
+    try {
+      final user = _authentication.currentUser;
+      if (user != null) {
+        loggedUser = user;
+        print(loggedUser!.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
