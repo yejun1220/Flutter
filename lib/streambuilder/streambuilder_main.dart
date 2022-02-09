4989908b97b3d73de81d4851e43432df825c7476
yejun_1220@naver.com <yejun_1220@naver.com>
2022-02-10 01:05:21 +0900

    package명 변경

--- lib/streambuilder/streambuilder_main.dart
+++ lib/streambuilder/streambuilder_main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study/StreamBuilder/counter.dart';
+import 'package:flutter_study/streambuilder/counter.dart';
 
 void main() {
   runApp(MyApp());
