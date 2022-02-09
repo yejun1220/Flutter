6c42b928a91811c7343a46570af95681eeff3ae5
yejun_1220@naver.com <yejun_1220@naver.com>
2022-02-10 01:04:47 +0900

    package명 변경

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_study/StreamBuilder/strreambuilder_main.dart';
+import 'package:flutter_study/streambuilder/streambuilder_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
