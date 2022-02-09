aa1bf25b57ec9a93fc98402c51461a80da247076
yejun_1220@naver.com <yejun_1220@naver.com>
2022-02-10 01:03:00 +0900

    package명 변경
    pubspec.yaml 및 packages 추가

--- lib/chatting/main.dart
+++ lib/chatting/main.dart
@@ -14,19 +14,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: NewMessage()
-        // home: Home()
+        home: Home()
     );
   }
 }
 
-// class Home extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text('Demo'),
-//         ),
-//         body: NewMessage());
-//   }
-// }
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Demo'),
+        ),
+        body: NewMessage());
+  }
+}
